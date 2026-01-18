d0b61b2a12616e3e6164f3be7a2fde4a9abb1488
daid <161341640+daiddayL@users.noreply.github.com>
2026-01-18 16:29:37 +0300

    performed task 2 of lesson 10 homework

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -12,3 +12,13 @@ test('Вам нужно написать тест, который проверя
 
   await expect(page).toHaveURL(/inventory\.html/);
 });
+
+test('Напишите еще один тест в том же файле, который проверяет сценарий неуспешного входа с заблокированным пользователем', async ({ page }) =>{
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.fill('#user-name','locked_out_user')
+  await page.fill('#password','secret_sauce')
+  await page.click('#login-button')
+
+  await expect(page.locator('body')).toContainText('Epic sadface: Sorry, this user has been locked out.');
+});
