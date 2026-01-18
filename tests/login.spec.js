00e1612eee0de3f502d9cf04f296b5411f71856c
daid <161341640+daiddayL@users.noreply.github.com>
2026-01-18 16:13:54 +0300

    performed the first task of lesson 10 homework

---
+++ tests/login.spec.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+
+
+test('Вам нужно написать тест, который проверяет успешный вход в систему', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.fill('#user-name','standard_user')
+  await page.fill('#password','secret_sauce')
+  await page.click('#login-button')
+
+  await expect(page).toHaveURL(/inventory\.html/);
+});
